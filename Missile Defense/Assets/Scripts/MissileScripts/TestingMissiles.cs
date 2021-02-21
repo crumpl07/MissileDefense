468e89dfdd1266738c63f83bd12cb9cde375fe6c
crumpl07 <crumpst3r2000@gmail.com>
2021-02-20 20:19:50 -0500

    Struggling with Missile Collision

--- Missile Defense/Assets/Scripts/MissileScripts/TestingMissiles.cs
+++ Missile Defense/Assets/Scripts/MissileScripts/TestingMissiles.cs
@@ -7,11 +7,10 @@ public class TestingMissiles : MonoBehaviour
     private Rigidbody rb;
     public GameObject EnemyMissile;
     public GameObject Terrain;
+    private GameObject Explosion;
     public float enemyVelocity;
     private Vector3 colPoint;
-    private Vector3 colVelocity;
-    private Vector3 colAngle;
-    private float distanceToCol;
+    private float colDistance;
 
     void Start()
     {
@@ -19,7 +18,11 @@ public class TestingMissiles : MonoBehaviour
         if (EnemyMissile != null)
         {
             EnemyMissile = GameObject.Find("EnemyMissile(Clone)");
-            GameObject Explosion = GameObject.Find("BigExplosionEffect(Clone)");
+            Explosion = GameObject.Find("BigExplosionEffect(Clone)");
+            if(Explosion != null)
+            {
+                Debug.Log("Explosion: " + Explosion.transform.position);
+            }
             Destroy(Explosion);
         }
 
@@ -28,27 +31,19 @@ public class TestingMissiles : MonoBehaviour
         //Find the colPoint
         colPoint.x = EnemyMissile.transform.position.x;
         colPoint.y = EnemyMissile.transform.position.y - (9.8f * 3);
-        colPoint.z = 1000 - enemyVelocity * 3;
-
-        //Set the velocityVector
-        colVelocity.x = colPoint.x / 3;
-        colVelocity.y = colPoint.y / 3;
-        colVelocity.z = colPoint.z / 3;
-
-        //Find the colAngle
-        colAngle.y = Mathf.Tan(colPoint.x / 450);
-        distanceToCol = Mathf.Sqrt(Mathf.Pow(450, 2) + Mathf.Pow(Mathf.Abs(colPoint.x - 500), 2));
-        colAngle.x = Mathf.Tan(colPoint.y / distanceToCol);
-
-        rb.velocity = colVelocity;
-        rb.transform.Rotate(colAngle);
-
+        colPoint.z = 1000 - (enemyVelocity * 3);
+        colDistance = Mathf.Sqrt(Mathf.Pow(colPoint.x - 500, 2) + Mathf.Pow(colPoint.y - 43, 2) + Mathf.Pow(colPoint.z - 100, 2));
+        //Debug.Log(colDistance);
+        Debug.Log("Collsion Point: " + colPoint);
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        rb.velocity = rb.transform.forward * (colDistance / 3);
+        rb.transform.LookAt(EnemyMissile.transform.position);
         if (transform.position.z < Terrain.transform.position.z ||
             transform.position.z > Terrain.transform.position.z + 1000)
         {

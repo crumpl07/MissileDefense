c065a840164f673ff508c397e641a34fdd95b6b5
crumpl07 <crumpst3r2000@gmail.com>
2021-05-17 10:45:52 -0400

    Added Drone

---
+++ Missile Defense/Assets/Scripts/PlaneScripts/DroneSpawn.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneSpawn : MonoBehaviour
+{
+    public GameObject Terrain;
+    public GameObject DronePrefab;
+    public int height;
+    private int spawnTime = 12;
+    void Start()
+    {
+        StartCoroutine(planeWave());
+    }
+
+    private void spawnDrone()
+    {
+        int randNum = 0;
+        randNum = Random.Range(1, 2);
+        switch (randNum)
+        {
+            case 1:
+                SpawnRight(DronePrefab);
+                break;
+            case 2:
+                SpawnLeft(DronePrefab);
+                break;
+        }
+    }
+
+    IEnumerator missileWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+            spawnDrone();
+        }
+    }
+
+    void SpawnLeft(GameObject enemyMissile)
+    {
+        enemyMissile.transform.position = (new Vector3(
+                                                       Terrain.transform.position.x,
+                                                       Terrain.transform.position.y + height,
+                                                       Random.Range(Terrain.transform.position.z + 1000, Terrain.transform.position.z + 3000)));
+    }
+
+    void SpawnRight(GameObject enemyMissile)
+    {
+        enemyMissile.transform.position = (new Vector3(
+                                                        Terrain.transform.position.x + 3000,
+                                                        Terrain.transform.position.y + height,
+                                                        Random.Range(Terrain.transform.position.z + 1000, Terrain.transform.position.z + 3000)));
+    }
+
+    IEnumerator planeWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+            spawnDrone();
+        }
+    }
+}
---
+++ Missile Defense/Assets/Scripts/PlaneScripts/FlyDrone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyDrone : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = rb.transform.forward * speed;
+        
+    }
+}
